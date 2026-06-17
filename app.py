1a01ca3128a685792dcde7a07dcbc9d1b358603d
RAMDAYAL BISHNOI <ramdayalbishnoi@RAMDAYALs-MacBook-Air.local>
2026-06-17 23:48:11 +0530

    add all files with folders

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, request, jsonify, render_template
 import joblib
 import numpy as np
 
-app = Flask(__name__)
+
 app = Flask(__name__, static_folder="static")
 
 # Load model
